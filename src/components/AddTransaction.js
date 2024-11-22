c1941a53778fb05c870264c56c325cdaca56a20b
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-11-23 00:30:59 +0530

    c25

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -13,7 +13,7 @@ const AddTransaction = () => {
     const newTransaction = {
       id: Math.floor(Math.random() * 100000000),
       text,
-      amount
+      amount: +amount
     }
 
     addTransaction(newTransaction);
